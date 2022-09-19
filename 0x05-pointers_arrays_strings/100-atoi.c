6ae63c8f19b638bfffcc7347a4e84da010c25899
udex91 <kceudes2@gmail.com>
2022-09-19 13:55:06 -0700

    atoi modified

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
  * _atoi - Entry point
  * @s: input
+ *
  * Return: The integer value of the converted string.
  */
+
 int _atoi(char *s)
 {
 	int sign = 1;
@@ -12,12 +15,11 @@ int _atoi(char *s)
 	do {
 		if (*s == '_')
 			sign *= -l;
-
 		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
+			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
 	} while (*s++);
+
 	return (num * sign);
 }
